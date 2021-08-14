b1dfaf5d301db2c5d4bd4e33691af8faa47547ac
MohamedMoustafaNUIG <mohamedmoustafaofficial@gmail.com>
2021-08-14 19:05:39 +0100

    Cleaned generated code and added various functionalities.
    Changes:
    1. Changed window title and added icon.
    2. Added the functions for output calculations for the two layers (from
       Main.py). Still need to connect GUI inputs with the functions.
    3. The "clear" button now works.
    4. Padding input is disabled if pooling layer is chosen.

--- GUI.py
+++ GUI.py
@@ -3,12 +3,15 @@ from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_MainWindow(object):
     def setupUi(self, MainWindow):
-        MainWindow.setObjectName("MainWindow")
+        MainWindow.setObjectName("ConvNet Calculator")
         MainWindow.resize(698, 335)
-        
+        MainWindow.setWindowIcon(QtGui.QIcon('Assets/NN_Icon.png'))
+
         self.centralwidget = QtWidgets.QWidget(MainWindow)
         self.centralwidget.setObjectName("centralwidget")
         
+        self.text_boxes = []
+
         self.line = QtWidgets.QFrame(self.centralwidget)
         self.line.setGeometry(QtCore.QRect(10, 190, 421, 21))
         self.line.setFrameShape(QtWidgets.QFrame.HLine)
@@ -27,7 +30,8 @@ class Ui_MainWindow(object):
         self.comboBox.setObjectName("comboBox")
         self.comboBox.addItem("")
         self.comboBox.addItem("")
-        
+        self.comboBox.currentIndexChanged.connect(self.on_layer_changed, self.comboBox.currentIndex())
+
         self.label = QtWidgets.QLabel(self.centralwidget)
         self.label.setGeometry(QtCore.QRect(160, 10, 91, 30))
         self.label.setObjectName("label")
@@ -39,11 +43,13 @@ class Ui_MainWindow(object):
         self.kernel_num_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.kernel_num_txt.setGeometry(QtCore.QRect(30, 150, 104, 31))
         self.kernel_num_txt.setObjectName("kernel_num_txt")
-        
+        self.text_boxes.append(self.kernel_num_txt)
+
         self.kernel_size_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.kernel_size_txt.setGeometry(QtCore.QRect(150, 150, 104, 31))
         self.kernel_size_txt.setObjectName("kernel_size_txt")
-        
+        self.text_boxes.append(self.kernel_size_txt)
+
         self.label_3 = QtWidgets.QLabel(self.centralwidget)
         self.label_3.setGeometry(QtCore.QRect(170, 120, 71, 21))
         self.label_3.setObjectName("label_3")
@@ -59,11 +65,13 @@ class Ui_MainWindow(object):
         self.padding_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.padding_txt.setGeometry(QtCore.QRect(220, 80, 104, 31))
         self.padding_txt.setObjectName("padding_txt")
-        
+        self.text_boxes.append(self.padding_txt)
+
         self.stride_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.stride_txt.setGeometry(QtCore.QRect(270, 150, 104, 31))
         self.stride_txt.setObjectName("stride_txt")
-        
+        self.text_boxes.append(self.stride_txt)
+
         self.label_6 = QtWidgets.QLabel(self.centralwidget)
         self.label_6.setGeometry(QtCore.QRect(160, 210, 91, 30))
         self.label_6.setObjectName("label_6")
@@ -83,23 +91,28 @@ class Ui_MainWindow(object):
         self.inp_width_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.inp_width_txt.setGeometry(QtCore.QRect(23, 270, 101, 31))
         self.inp_width_txt.setObjectName("inp_width_txt")
-        
+        self.text_boxes.append(self.inp_width_txt)
+
         self.inp_height_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.inp_height_txt.setGeometry(QtCore.QRect(150, 270, 101, 31))
         self.inp_height_txt.setObjectName("inp_height_txt")
-        
+        self.text_boxes.append(self.inp_height_txt)
+
         self.inp_depth_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.inp_depth_txt.setGeometry(QtCore.QRect(280, 270, 101, 31))
         self.inp_depth_txt.setObjectName("inp_depth_txt")
-        
+        self.text_boxes.append(self.inp_depth_txt)
+
         self.calculateButton = QtWidgets.QPushButton(self.centralwidget)
         self.calculateButton.setGeometry(QtCore.QRect(460, 260, 93, 28))
         self.calculateButton.setObjectName("calculateButton")
+        self.calculateButton.clicked.connect(self.calculate)
 
         self.clearButton = QtWidgets.QPushButton(self.centralwidget)
         self.clearButton.setGeometry(QtCore.QRect(580, 260, 93, 28))
         self.clearButton.setObjectName("clearButton")
-        
+        self.clearButton.clicked.connect(self.clear)
+
         self.label_10 = QtWidgets.QLabel(self.centralwidget)
         self.label_10.setGeometry(QtCore.QRect(510, 10, 101, 30))
         self.label_10.setObjectName("label_10")
@@ -160,10 +173,52 @@ class Ui_MainWindow(object):
         self.label_11.setText(_translate("MainWindow", "Width"))
         self.label_12.setText(_translate("MainWindow", "Height"))
         self.label_13.setText(_translate("MainWindow", "Depth"))
-        self.out_width_label.setText(_translate("MainWindow", "N/A"))
-        self.out_height_label.setText(_translate("MainWindow", "N/A"))
-        self.out_depth_label.setText(_translate("MainWindow", "N/A"))
+        self.out_width_label.setText(_translate("MainWindow", ""))
+        self.out_height_label.setText(_translate("MainWindow", ""))
+        self.out_depth_label.setText(_translate("MainWindow", ""))
+    
+    def calculate(self):
+        _translate = QtCore.QCoreApplication.translate
 
+        self.out_width_label.setText(_translate("MainWindow", self.inp_width_txt.toPlainText()))
+        self.out_height_label.setText(_translate("MainWindow", self.inp_height_txt.toPlainText()))
+        self.out_depth_label.setText(_translate("MainWindow", self.inp_depth_txt.toPlainText()))
+        self.update()
+    
+    def clear(self):
+        _translate = QtCore.QCoreApplication.translate
+
+        self.out_width_label.setText(_translate("MainWindow", ""))
+        self.out_height_label.setText(_translate("MainWindow", ""))
+        self.out_depth_label.setText(_translate("MainWindow", ""))
+
+        for i in range(len(self.text_boxes)):
+            self.text_boxes[i].setPlainText(_translate("MainWindow", ""))
+
+        self.update()
+        
+    def update(self):
+        self.out_width_label.adjustSize()
+        self.out_height_label.adjustSize()
+        self.out_depth_label.adjustSize()
+
+    def on_layer_changed(self, value):
+        if value == 0:
+            self.padding_txt.setDisabled(False)
+        else:
+            self.padding_txt.setDisabled(True)
+
+    def conv_layer(self, in_h, in_w, in_d, kernels, kernel_size, padding, stride):
+        new_h = int((in_h-kernel_size+(2*padding))/stride)+1
+        new_w = int((in_w-kernel_size+(2*padding))/stride)+1
+        new_d = kernels
+        return new_h, new_w, new_d
+
+    def pooling_layer(self, in_h, in_w, in_d, window_size, stride):
+        new_h = int((in_h-window_size)/stride)+1
+        new_w = int((in_w-window_size)/stride)+1
+        new_d = in_d
+        return new_h, new_w, new_d
 
 if __name__ == "__main__":
     import sys
--- README.md
+++ README.md
@@ -6,11 +6,15 @@ An application to calculate the output dimensions of a layer in a traditional co
   <li><s>Design a command line program for handling simple conv and pool layers</s></li>
   <li><s>Design basic GUI for inputs (using Qt Designer)</s></li>
   <li><s>Clean up the code generated from Qt Designer</s></li>
-  <li>Start testing attaching commands to buttons and manipulating textboxes and labels</li>
+  <li><s>Start testing attaching commands to buttons and manipulating textboxes and labels</s></li>
   <li>Add the logic code to the GUI</li>
   <li>(Optional) Improve GUI, work on error handling</li>
   <li>(For the future) Allow for multi-layer input-output</li>
 </ol>
 
 # GUI prototype preview
-![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/GUI_proto.PNG?raw=true)
+<h3><b>Convolutional Layer</b></h3>
+![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/Assets/GUI_conv.PNG?raw=true)
+
+<h3><b>Pooling Layer</b></h3>
+![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/Assets/GUI_pool.PNG?raw=true)
