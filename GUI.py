f1f0300f158a75fd0fb5c84c47772d2c72994ac1
MohamedMoustafaNUIG <mohamedmoustafaofficial@gmail.com>
2021-08-18 07:08:32 +0100

    Finished connecting the logic with the inputs to the GUI; Added error handling message; Added comments to the code; Started disabling "kernel number" when pooling layer chosen.

--- GUI.py
+++ GUI.py
@@ -24,6 +24,7 @@ class Ui_MainWindow(object):
         self.line_2.setFrameShadow(QtWidgets.QFrame.Sunken)
         self.line_2.setObjectName("line_2")
         
+        #drop down list to choose type of layer
         self.comboBox = QtWidgets.QComboBox(self.centralwidget)
         self.comboBox.setGeometry(QtCore.QRect(30, 60, 111, 31))
         self.comboBox.setMaximumSize(QtCore.QSize(111, 31))
@@ -40,11 +41,13 @@ class Ui_MainWindow(object):
         self.label_2.setGeometry(QtCore.QRect(30, 120, 111, 21))
         self.label_2.setObjectName("label_2")
         
+        #input for number of kernels; for convolutional layers
         self.kernel_num_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.kernel_num_txt.setGeometry(QtCore.QRect(30, 150, 104, 31))
         self.kernel_num_txt.setObjectName("kernel_num_txt")
         self.text_boxes.append(self.kernel_num_txt)
 
+        #input for kernel size
         self.kernel_size_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.kernel_size_txt.setGeometry(QtCore.QRect(150, 150, 104, 31))
         self.kernel_size_txt.setObjectName("kernel_size_txt")
@@ -62,11 +65,13 @@ class Ui_MainWindow(object):
         self.label_5.setGeometry(QtCore.QRect(300, 120, 41, 21))
         self.label_5.setObjectName("label_5")
         
+        #input to specify padding (if any); for convolutional layer
         self.padding_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.padding_txt.setGeometry(QtCore.QRect(220, 80, 104, 31))
         self.padding_txt.setObjectName("padding_txt")
         self.text_boxes.append(self.padding_txt)
 
+        #input to specify stride
         self.stride_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.stride_txt.setGeometry(QtCore.QRect(270, 150, 104, 31))
         self.stride_txt.setObjectName("stride_txt")
@@ -88,6 +93,7 @@ class Ui_MainWindow(object):
         self.label_9.setGeometry(QtCore.QRect(315, 245, 41, 21))
         self.label_9.setObjectName("label_9")
         
+        #text boxes to input input dimensions
         self.inp_width_txt = QtWidgets.QTextEdit(self.centralwidget)
         self.inp_width_txt.setGeometry(QtCore.QRect(23, 270, 101, 31))
         self.inp_width_txt.setObjectName("inp_width_txt")
@@ -103,11 +109,13 @@ class Ui_MainWindow(object):
         self.inp_depth_txt.setObjectName("inp_depth_txt")
         self.text_boxes.append(self.inp_depth_txt)
 
+        #button to calculate output dimensions
         self.calculateButton = QtWidgets.QPushButton(self.centralwidget)
         self.calculateButton.setGeometry(QtCore.QRect(460, 260, 93, 28))
         self.calculateButton.setObjectName("calculateButton")
         self.calculateButton.clicked.connect(self.calculate)
 
+        #button to clear text boxes and some labels
         self.clearButton = QtWidgets.QPushButton(self.centralwidget)
         self.clearButton.setGeometry(QtCore.QRect(580, 260, 93, 28))
         self.clearButton.setObjectName("clearButton")
@@ -129,6 +137,7 @@ class Ui_MainWindow(object):
         self.label_13.setGeometry(QtCore.QRect(510, 150, 51, 21))
         self.label_13.setObjectName("label_13")
         
+        #labels used to display calculated output
         self.out_width_label = QtWidgets.QLabel(self.centralwidget)
         self.out_width_label.setGeometry(QtCore.QRect(600, 70, 55, 21))
         self.out_width_label.setObjectName("out_width_label")
@@ -141,6 +150,11 @@ class Ui_MainWindow(object):
         self.out_depth_label.setGeometry(QtCore.QRect(600, 150, 55, 21))
         self.out_depth_label.setObjectName("out_depth_label")
         
+        #label to display error message if calculation fails
+        self.error_label = QtWidgets.QLabel(self.centralwidget)
+        self.error_label.setGeometry(QtCore.QRect(450, 220, 101, 30))
+        self.error_label.setObjectName("error_label")
+
         MainWindow.setCentralWidget(self.centralwidget)
         self.statusbar = QtWidgets.QStatusBar(MainWindow)
         self.statusbar.setObjectName("statusbar")
@@ -176,45 +190,66 @@ class Ui_MainWindow(object):
         self.out_width_label.setText(_translate("MainWindow", ""))
         self.out_height_label.setText(_translate("MainWindow", ""))
         self.out_depth_label.setText(_translate("MainWindow", ""))
-    
+
     def calculate(self):
         _translate = QtCore.QCoreApplication.translate
+        try:
+            h = int(self.inp_height_txt.toPlainText())
+            w = int(self.inp_width_txt.toPlainText())
+            d = int(self.inp_depth_txt.toPlainText())
+            
+            k_s = int(self.kernel_size_txt.toPlainText())
+            s = int(self.stride_txt.toPlainText())
+            
+            if self.comboBox.currentIndex() == 0:#if conv
+                k_n = int(self.kernel_num_txt.toPlainText())
+                p = int(self.padding_txt.toPlainText())
+                new_h, new_w, new_d = self.conv_layer(h, w, d, k_n, k_s, p, s)
+            else:#if pool
+                new_h, new_w, new_d = self.pooling_layer(h, w, d, k_s, s)
+            
+            self.out_height_label.setText(_translate("MainWindow", str(new_h)))
+            self.out_width_label.setText(_translate("MainWindow", str(new_w)))
+            self.out_depth_label.setText(_translate("MainWindow", str(new_d)))
+        except:#if error
+            self.error_label.setText(_translate("MainWindow", "Error! Fill all text boxes with integers!"))
+            self.update()
 
-        self.out_width_label.setText(_translate("MainWindow", self.inp_width_txt.toPlainText()))
-        self.out_height_label.setText(_translate("MainWindow", self.inp_height_txt.toPlainText()))
-        self.out_depth_label.setText(_translate("MainWindow", self.inp_depth_txt.toPlainText()))
-        self.update()
-    
     def clear(self):
         _translate = QtCore.QCoreApplication.translate
 
         self.out_width_label.setText(_translate("MainWindow", ""))
         self.out_height_label.setText(_translate("MainWindow", ""))
         self.out_depth_label.setText(_translate("MainWindow", ""))
+        self.error_label.setText(_translate("MainWindow", ""))
 
+        #empty all text boxes
         for i in range(len(self.text_boxes)):
             self.text_boxes[i].setPlainText(_translate("MainWindow", ""))
 
         self.update()
         
-    def update(self):
+    def update(self):#adjust sizes
         self.out_width_label.adjustSize()
         self.out_height_label.adjustSize()
         self.out_depth_label.adjustSize()
+        self.error_label.adjustSize()
 
-    def on_layer_changed(self, value):
+    def on_layer_changed(self, value):#disable/enable certain inputs depending on chosen layer
         if value == 0:
             self.padding_txt.setDisabled(False)
+            self.kernel_num_txt.setDisabled(False)
         else:
             self.padding_txt.setDisabled(True)
+            self.kernel_num_txt.setDisabled(True)
 
-    def conv_layer(self, in_h, in_w, in_d, kernels, kernel_size, padding, stride):
+    def conv_layer(self, in_h, in_w, in_d, kernels, kernel_size, padding, stride):#calculate output dimension of convolutional layer
         new_h = int((in_h-kernel_size+(2*padding))/stride)+1
         new_w = int((in_w-kernel_size+(2*padding))/stride)+1
         new_d = kernels
         return new_h, new_w, new_d
 
-    def pooling_layer(self, in_h, in_w, in_d, window_size, stride):
+    def pooling_layer(self, in_h, in_w, in_d, window_size, stride):#calculate output dimension of pooling layer
         new_h = int((in_h-window_size)/stride)+1
         new_w = int((in_w-window_size)/stride)+1
         new_d = in_d
--- README.md
+++ README.md
@@ -7,15 +7,19 @@ An application to calculate the output dimensions of a layer in a traditional co
   <li><s>Design basic GUI for inputs (using Qt Designer)</s></li>
   <li><s>Clean up the code generated from Qt Designer</s></li>
   <li><s>Start testing attaching commands to buttons and manipulating textboxes and labels</s></li>
-  <li>Add the logic code to the GUI</li>
-  <li>(Optional) Improve GUI, work on error handling</li>
+  <li><s>Add the logic code to the GUI</s></li>
+  <li><s>Add comments</s></li>
+  <li><s>(Optional) Improve GUI, work on error handling</s></li>
   <li>(For the future) Allow for multi-layer input-output</li>
 </ol>
 
-# GUI prototype preview
+# GUI Preview
 ### Convolutional Layer
 
 ![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/Assets/GUI_conv.PNG)
 
 ### Pooling Layer
 ![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/Assets/GUI_pool.PNG?raw=true)
+
+### Error Notification
+![GUI prototype](https://github.com/MohamedMoustafaNUIG/ConvNet-Dimension-Calculator/blob/main/Assets/GUI_error.PNG?raw=true)
